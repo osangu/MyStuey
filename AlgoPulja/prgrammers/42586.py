966a0fb08d7e949d3d52683030336f92f4647558
cogofoward <hhgoon@naver.com>
2022-04-18 16:00:10 +0900

    finish :: 42586

--- AlgoPulja/prgrammers/42586.py
+++ AlgoPulja/prgrammers/42586.py
@@ -3,7 +3,6 @@ import math
 
 def solution(progresses, speeds):
     day = []
-    answer = []
     for i in range(0, len(progresses)):
         progresses[i] = 100 - progresses[i]
 
@@ -12,21 +11,24 @@ def solution(progresses, speeds):
         else:
             day.append(math.trunc((progresses[i] / speeds[i])) + 1)
 
-    a = day[0]
-    count = 1
-    # [7, 3, 9]
-    # [5, 10, 1, 1, 20, 1]
+    answer = []
+    count = 0
+    num = day[0]
 
     for i in range(1, len(day)):
-        if a >= day[i]:
-            a = day[i]
-            count += 1
+        count += 1
+        if num >= day[i]:
             continue
         else:
-            a = day[i]
+            num = day[i]
             answer.append(count)
-            count = 1
-
-    answer.append(count)
-
+            count = 0
+    answer.append(count + 1)
     return answer
+
+
+if __name__ == '__main__':
+    solution([93, 30, 55], [1, 30, 5])
+    solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1])
+    solution([20, 99, 93, 30, 55, 10], [5, 10, 1, 1, 30, 5])
+    solution([1, 99], [99, 1])
